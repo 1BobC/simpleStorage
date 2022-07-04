f47677450735e93dceb485ca1f250f79b38934bb
1BObC <bob.cocksedge@gmail.com>
2022-07-04 09:03:20 +0100

    1st vscode direct to git update

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,4 +1,5 @@
 //first go at a smart contract!
+//also first go at vscode direct to git update!
 
 //Search compile deploy and run solidity transactions in vscode
 
@@ -8,28 +9,27 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
-
+    //this will get initiallised as zero.
     uint256 favoriteNumber;
 
-    // This is a comment!
-    struct People {
-        uint256 favoriteNumber;
-        string name;
-    }
+    // struct People {
+    //     uint256 favoriteNumber;
+    //     string name;
+    // }
 
-    People[] public people;
-    mapping(string => uint256) public nameToFavoriteNumber;
+    // People[] public people;
+    // mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
     
-    function retrieve() public view returns (uint256){
-        return favoriteNumber;
-    }
+    // function retrieve() public view returns (uint256){
+    //     return favoriteNumber;
+    // }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
+    // function addPerson(string memory _name, uint256 _favoriteNumber) public {
+    //     people.push(People(_favoriteNumber, _name));
+    //     nameToFavoriteNumber[_name] = _favoriteNumber;
+    // }
 }
